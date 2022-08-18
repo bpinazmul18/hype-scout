8a5b3e511dc920e2868df0458311b7982aa5ea33
Nazmul Haque <bpinazmul18@gmail.com>
2022-08-18 21:23:58 +0600

    Add react-router-dom, pages and not-found page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom'
+
+import Home from './pages/Home';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+
 import './App.scss';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <NavBar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+
+        <Route path='not-found' element={<p>NOT FOUND</p>}/>
+        <Route path='*' element={<Navigate to="/not-found" replace/>}/>
+      </Routes>
+      <Footer/>
+    </>
   );
 }
 
